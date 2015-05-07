70b07c5577559ab35cef4592e2bc8b4e0a040d98
Kaic Bastidas <tcK1@users.noreply.github.com>
2015-05-07 15:44:50 -0300

    Update ex1.sql

--- exercicios/ex1.sql
+++ exercicios/ex1.sql
@@ -1,3 +1,5 @@
 ﻿create view ex1 as
 select nome, count(*) from artista, objetos_arte where artista.id_artista = objetos_arte.id_artista
 group by artista.nome
+
+select artista.nome, count from ex1, artista where artista.nome = ex1.nome and artista.paisorigem='Italia'
