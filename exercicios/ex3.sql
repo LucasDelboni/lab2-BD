eaba1aadb280e58c4477b588c770f811d85549f0
Lucas Delboni <lucas_cd460@hotmail.com>
2015-05-08 23:28:43 -0300

    Descobri como faz para retornar tabela.
    O ex3 ainda nao esta pronto.

--- exercicios/ex3.sql
+++ exercicios/ex3.sql
@@ -1,7 +1,11 @@
-﻿create or replace function ex3 (char) returns table( id_objeto_arte, titulo) as --> colocar para retornar uma view
-select id_objeto_arte, titulo from objetos_arte where id_exposicoes = (
-select id_exposicoes from exposicoes where nome=$1);
+﻿-->Crie uma outra função que dado o nome de uma coleção retorne o NumId e Título das pinturas emprestadas ao museu por essa coleção.
+
+create or replace function ex3 (char) returns table (id_objeto_arte numeric, titulo char) as $$
+select id_objeto_arte, titulo from objetos_arte where id_objeto_arte = (
+-->pegar id_objeto_arte a partir da id_colecoes
+select id_colecoes_ex from colecoes_externa where nome=$1)
+--> pegar id_colecao das colecoes permanentes tambem
+;
 $$ language sql;
 
-select ex3('O Modernismo' )-->o renascimento pode ser mudado para outra exposicao
---> o modernismo e exposaicao numero 2
+select ex3('Colecao Renascimento' )-->o renascimento pode ser mudado para outra exposicao
