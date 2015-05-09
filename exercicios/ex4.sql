ed1c5f2cebc113c7d3c2dd7de1ac305f85a3b63c
dnsunao <denis.shiroma@gmail.com>
2015-05-09 12:31:37 -0300

    Exercicio 4 & 5
    Feitos:
    Ex 4 primeira parte,
    Ex 5 somente o update funciona

---
+++ exercicios/ex4.sql
@@ -0,0 +1,68 @@
+--trigger Remocao
+DROP TRIGGER remova_pintura ON PINTURA;
+DROP FUNCTION remova_pintura();
+
+CREATE FUNCTION remova_pintura() RETURNS trigger AS $$
+	BEGIN
+		DELETE FROM PINTURA
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		DELETE FROM TIPO
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		DELETE FROM COLECAO_PERMANENTE
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		DELETE FROM EMPRESTADO
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		DELETE FROM PROPRIEDADE
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte; 
+		
+		DELETE FROM OBJETOS_ARTE
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		RETURN NULL;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER remova_pintura AFTER DELETE ON PINTURA
+FOR EACH ROW EXECUTE PROCEDURE remova_pintura();
+
+--trigger Atualizacao NumId
+
+DROP TRIGGER atualiza_pintura ON PINTURA;
+DROP FUNCTION atualiza_pintura();
+
+CREATE FUNCTION atualiza_pintura() RETURNS trigger AS $$
+	BEGIN
+		UPDATE PINTURA
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		UPDATE FROM TIPO
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		UPDATE FROM COLECAO_PERMANENTE
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		UPDATE FROM EMPRESTADO
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		UPDATE FROM PROPRIEDADE
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte; 
+		
+		UPDATE FROM OBJETOS_ARTE
+		SET Id_Objeto_Arte = NEW.Id_Objeto_Arte
+		WHERE Id_Objeto_Arte = OLD.Id_Objeto_Arte;
+		
+		RETURN NULL;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER atualiza_pintura AFTER UPDATE ON PINTURA
+FOR EACH ROW EXECUTE PROCEDURE atualiza_pintura();
---
+++ exercicios/ex5.sql
@@ -0,0 +1,31 @@
+--Trigger CustoTotal
+
+CREATE FUNCTION atualiza_custo_total() RETURNS trigger AS $$
+	BEGIN
+		IF (TG_OP = 'DELETE') THEN
+			UPDATE COLECOES_EXTERNAS 
+			SET Custo_Total = Custo_Total - OLD.Custo
+			WHERE Id_Colecoes_Ex IN (SELECT CE.Id_Colecoes_Ex
+									FROM COLECOES_EXTERNAS AS CE , EMPRESTADO AS E
+									WHERE CE.Id_Colecoes_Ex = E.Id_Colecoes_Ex AND E.id_Objeto_Arte = OLD.id_Objeto_Arte);
+		END IF;
+		IF (TG_OP = 'UPDATE') THEN
+			UPDATE COLECOES_EXTERNAS 
+			SET Custo_Total = (Custo_Total + (NEW.Custo - OLD.Custo)) 
+			WHERE Id_Colecoes_Ex IN (SELECT CE.Id_Colecoes_Ex
+									FROM COLECOES_EXTERNAS AS CE , EMPRESTADO AS E
+									WHERE CE.Id_Colecoes_Ex = E.Id_Colecoes_Ex AND E.id_Objeto_Arte = OLD.id_Objeto_Arte);
+		END IF;
+		IF (TG_OP = 'INSERT') THEN
+			UPDATE COLECOES_EXTERNAS 
+				SET Custo_Total = Custo_Total + NEW.Custo
+				WHERE Id_Colecoes_Ex IN (SELECT CE.Id_Colecoes_Ex
+										FROM COLECOES_EXTERNAS AS CE , EMPRESTADO AS E
+										WHERE CE.Id_Colecoes_Ex = E.Id_Colecoes_Ex AND E.id_Objeto_Arte = OLD.id_Objeto_Arte);
+		END IF;
+		RETURN NULL;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER atualiza_custo_total AFTER INSERT OR UPDATE OR DELETE ON OBJETOS_ARTE
+FOR EACH ROW EXECUTE PROCEDURE atualiza_custo_total();
