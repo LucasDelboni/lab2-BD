4039623d51f31a38dbc344fd89caf0089cb5abf3
Lucas Delboni <lucas_cd460@hotmail.com>
2015-05-10 17:54:04 -0300

    Exercicio 3 finalizado

--- exercicios/ex3.sql
+++ exercicios/ex3.sql
@@ -1,7 +1,7 @@
 ﻿-->Crie uma outra função que dado o nome de uma coleção retorne o NumId e Título das pinturas emprestadas ao museu por essa coleção.
 
 create or replace function ex3 (char) returns table (id_objeto_arte numeric, titulo char) as $$
-select id_objeto_arte, titulo from objetos_arte where id_objeto_arte = (
+select id_objeto_arte, titulo from objetos_arte where id_objeto_arte IN (
 select id_objeto_arte from emprestado where id_colecoes_ex = (
 select id_colecoes_ex from colecoes_externas where nome = $1));
 $$ language sql;
