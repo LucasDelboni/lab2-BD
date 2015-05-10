b45a88dc3457f8de7ac9a603ed951021c7b6c560
Lucas Delboni <lucas_cd460@hotmail.com>
2015-05-10 17:39:40 -0300

    3 exercicio quase pronto

--- exercicios/ex3.sql
+++ exercicios/ex3.sql
@@ -2,10 +2,8 @@
 
 create or replace function ex3 (char) returns table (id_objeto_arte numeric, titulo char) as $$
 select id_objeto_arte, titulo from objetos_arte where id_objeto_arte = (
--->pegar id_objeto_arte a partir da id_colecoes
-select id_colecoes_ex from colecoes_externa where nome=$1)
---> pegar id_colecao das colecoes permanentes tambem
-;
+select id_objeto_arte from emprestado where id_colecoes_ex = (
+select id_colecoes_ex from colecoes_externas where nome = $1));
 $$ language sql;
 
 select ex3('Colecao Renascimento' )-->o renascimento pode ser mudado para outra exposicao
